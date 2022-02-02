e4e2bf3c814970f1c6f3213213cc0e64597e0ff1
MuhammadYaumil1212 <yaumilramkhadani@gmail.com>
2022-02-03 01:26:50 +0700

    slicing kerangka

--- app/Http/Controllers/Landing/LandingController.php
+++ app/Http/Controllers/Landing/LandingController.php
@@ -14,7 +14,7 @@ class LandingController extends Controller
      */
     public function index()
     {
-        return "Hello World";
+        return view('pages.landing.index');
     }
 
     /**
