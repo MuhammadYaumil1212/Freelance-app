858ed0c15ec65fe060d766cd2c380097766ea979
MuhammadYaumil1212 <yaumilramkhadani@gmail.com>
2022-02-05 13:48:47 +0700

    add csrf token in meta file

--- resources/views/includes/dashboard/meta.blade.php
+++ resources/views/includes/dashboard/meta.blade.php
@@ -2,3 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="icon" type="image/png" sizes="32x32" href="{{asset('Assets/icons/favicon-32x32.png')}}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{asset('Assets/icons/favicon-16x16.png')}}">
+
+{{-- Csrf Token --}}
+<meta name="csrf-token" content="{{csrf_token()}}">
